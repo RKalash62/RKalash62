99cedd567b2303658f1b9b1b6f6d32082c891c49
RKalash62 <65034086+RKalash62@users.noreply.github.com>
2021-07-23 10:49:27 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    token: "process.env.TOKEN", // DO NOT SHARE WITH ANYONE!!
+    logging: {
+        enabled: true, // Only enable if you want the roles being added/removed to be logged
+        webhook: "https://discord.com/api/webhooks/867332660942077973/6vHJGiNhsEprWWplHWsaQ7VmN-HlOQ29qlzZYvxZrKHCf2NGgOOhJx_J4WIZaLMWpm5C" 
+    },
+    roles: [
+        { 
+             emoji: "✅", 
+             roleID: "866968120640143361", 
+             channelID: "866965170618236940", 
+             messageID: "867339962278215690",  
+         }
+    ]
+}
